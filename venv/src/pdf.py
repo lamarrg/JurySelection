79e65e73121dab69bc42733d54709355c29e7ae1
Lamar Greene <github@lamargreene.com>
2017-10-23 07:17:07 -0600

    Fixed error overwriting pdf
    pdf could be overwritten only once per application start. The issue was
    initiating the pdf feature once, instead of each time it was called.

--- venv/src/pdf.py
+++ venv/src/pdf.py
@@ -1,41 +1,47 @@
 from fpdf import FPDF
 import os
 
-pdf = FPDF(format='3x5', unit='in')
-pdf.add_page()
-pdf.set_font('Arial', '', 12) # pdf.set_font('Arial', 'B', 16)
-effective_page_width = pdf.w - 2*pdf.l_margin
+
+def set_pdf():
+    pdf_start = FPDF(format='3x5', unit='in')
+    pdf_start.add_page()
+    pdf_start.set_font('Arial', '', 12)  # pdf.set_font('Arial', 'B', 16)
+    return pdf_start
 
 
 def file_path_underscore(path_text):
     return path_text.lower().replace(" ", "_")
 
 
-def print_juror(juror):
+def print_juror(juror_data):
+    pdf = set_pdf()
+    effective_page_width = pdf.w - 2 * pdf.l_margin
+
     # cell for header-trial name
     pdf.set_font('Times', 'B', 12.0)
-    pdf.cell(0.0, 0.0, str(juror.trial).upper())
+    pdf.cell(0.0, 0.0, str(juror_data.trial).upper())
     pdf.ln(0.25)
 
     # cell for name
     pdf.set_font('Times', 'B', 10.0)
     pdf.cell(0.45, 0.0, 'Name: ')
     pdf.set_font('Times', '', 10.0)
-    pdf.cell(0.0, 0.0, juror.name, ln=0)
+    pdf.cell(0.0, 0.0, juror_data.name, ln=0)
     pdf.ln(0.2)
 
     # cell for age
     pdf.set_font('Times', 'B', 10.0)
     pdf.cell(0.35, 0.0, 'Age: ')
     pdf.set_font('Times', '', 10.0)
-    pdf.cell(0.0, 0.0, str(juror.age))
+    pdf.cell(0.0, 0.0, str(juror_data.age))
+    print("juror age {}".format(juror_data.age))
     pdf.ln(0.2)
 
     # cell for occupation
     pdf.set_font('Times', 'B', 10.0)
     pdf.cell(0.85, 0.0, 'Occupation:')
     pdf.set_font('Times', '', 10.0)
-    pdf.cell(0.0, 0.0, juror.occupation)
+    pdf.cell(0.0, 0.0, juror_data.occupation)
     pdf.ln(0.2)
 
     # cell for details. wraps data to fit width of card
@@ -43,11 +49,11 @@ def print_juror(juror):
     pdf.cell(0.0, 0.0, 'Details:')
     pdf.ln(0.10)
     pdf.set_font('Times', '', 10.0)
-    pdf.multi_cell(effective_page_width, 0.15, juror.details)
+    pdf.multi_cell(effective_page_width, 0.15, juror_data.details)
     pdf.ln(0.15)
 
-    trial_file_path = 'trials/'+file_path_underscore(juror.trial)
-    juror_file_path = file_path_underscore(juror.name)
+    trial_file_path = 'trials/'+file_path_underscore(juror_data.trial)
+    juror_file_path = file_path_underscore(juror_data.name)
     if not os.path.exists(trial_file_path):
         os.makedirs(trial_file_path)
 
@@ -57,10 +63,10 @@ def print_juror(juror):
 def remove_juror_pdf(juror):
     trial_file_path = 'trials/'+file_path_underscore(juror.trial)
     juror_file_path = file_path_underscore(juror.name)
-    pdf = trial_file_path+'/'+juror_file_path+".pdf"
-    if os.path.exists(pdf):
+    pdf_to_remove = trial_file_path+'/'+juror_file_path+".pdf"
+    if os.path.exists(pdf_to_remove):
         try:
-            os.remove(pdf)
+            os.remove(pdf_to_remove)
         except OSError:
             pass
 
